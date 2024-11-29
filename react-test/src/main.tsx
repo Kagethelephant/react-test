8d98c1db1078c42788ae8e685ee5323106b56f4e
Kagethelephant <kagethelephant@gmail.com>
2024-11-29 15:05:13 -0700

    initial

--- react-test/src/main.tsx
+++ react-test/src/main.tsx
@@ -3,7 +3,6 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
 
-// This is the main tsx file i think, usually named index.tsx?createRoot
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <App />
--- react/src/App.tsx
+++
@@ -1,9 +0,0 @@
-import { useState } from 'react'
-import './App.css'
-
-function App() {
-  const [count, setCount] = useState(0)
-
-  return ( <button onClick={() => setCount((count) => count + 3)}> count is {count} </button>)}
-
-export default App
